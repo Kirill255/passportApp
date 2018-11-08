b243894d5198331e410187a1513a29230a0e5d6b
Kirill255 <jhgbnm1@mail.ru>
2018-11-08 10:32:42 +0300

    fix sigint handler

--- data/mongoose.js
+++ data/mongoose.js
@@ -33,5 +33,13 @@ mongoose.connection.on("disconnected", function() {
 // 	});
 // });
 
+// вроде разобрался
+process.on("SIGINT", function() {
+	mongoose.connection.close(function() {
+		console.log("Mongoose default connection disconnected through app termination");
+		process.exit(0);
+	});
+});
+
 
 module.exports = mongoose;
